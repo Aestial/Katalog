b0751c083cf5063286e072b2864a9aa6a481e091
Jaime Hernandez <jaimevazz@gmail.com>
2020-11-29 18:41:45 -0600

    Label visibility relative to scene origin

--- webpack/src/siemens-logo-8/labelman.js
+++ webpack/src/siemens-logo-8/labelman.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
+import { labelman  as params } from './params';
+
 export default class LabelManager {
     constructor(container, camera, scene) {
         this.container = container;
@@ -9,13 +11,18 @@ export default class LabelManager {
         this.renderer = new CSS2DRenderer();
         this.dom = this.renderer.domElement;
         this.index = 0;
+        this.origin = new THREE.Vector3(params.origin.x, params.origin.y, params.origin.z);
+        this.elements = [];
+        this.configure();
+        // Events
+        window.addEventListener('resize', this.onWindowResize.bind(this), false);
+        this.onWindowResize();        
+    }
+    configure() {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.dom.style.position = 'absolute';
         this.dom.style.top = '0px';
         this.container.appendChild(this.dom);
-        // Events
-        window.addEventListener('resize', this.onWindowResize.bind(this), false);
-        this.onWindowResize();        
     }
     create(mesh, name, content) {
         const labelDiv = document.createElement('div');
@@ -28,6 +35,7 @@ export default class LabelManager {
         const label = new CSS2DObject(labelDiv);
         label.position.copy(mesh.position);
         mesh.add(label);
+        this.elements.push({ label, labelDiv });
     }
     displayInfo(name, content) {
         $('#rightModal').modal('show');
@@ -40,6 +48,13 @@ export default class LabelManager {
         this.renderer.setSize(w, h);
     }
     update() {
+        this.elements.forEach((elem) => {
+            const { label, labelDiv} = elem;
+            const meshDistance = this.camera.position.distanceTo(this.origin);
+            const labelDistance = this.camera.position.distanceTo(label.position);
+            const isBehindObject = labelDistance > meshDistance;
+            labelDiv.style.opacity = isBehindObject ? params.opacity.hidden : params.opacity.visible;
+        });
         this.renderer.render(this.scene, this.camera);
     }
 }
