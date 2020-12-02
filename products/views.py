061947fb14afe1ac9d268450fdb804da86246bf6
Jaime Hernandez <jaimevazz@gmail.com>
2020-12-01 23:00:08 -0600

    DRY product views: detail and dev

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ def all(request):
     products = Product.objects.all
     return render(request, 'products/all.html', {'section':'products', 'products':products})
 
-def detail(request, product_slug):
+def detail(request, product_slug, is_dev=False):
     product = get_object_or_404(Product, slug = product_slug)
     interactive = Interactive.objects.filter(product__id = product.id).first()
     assets = {asset.name:asset.file.url for asset in interactive.assets.all()}
@@ -24,24 +24,11 @@ def detail(request, product_slug):
         'assets':assets,
         'annotations':annotations,
         'annots_dict':annots_dict,
+        'dev':is_dev
     })
 
 def dev(request):
-    product = Product.objects.filter(name = "LOGO! 8").first()
-    interactive = Interactive.objects.filter(product__id = product.id).first()
-    assets = {asset.name:asset.file.url for asset in interactive.assets.all()}
-    annotations = interactive.annotations.all()
-    annots_dict = {a.index:{
-        "camPosition": a.camPosition, 
-        "title": a.title
-    } for a in annotations}
-    # annots_json = json.dumps(list(annotations), cls=DjangoJSONEncoder)
-    return render(request, 'products/detail.html', {
-        'section':'products',
-        'product':product,
-        'interactive':interactive,
-        'assets':assets,
-        'annotations':annotations,
-        'annots_dict':annots_dict,
-        'dev':True
-    })
+    return detail(request, "siemens-logo-8", True)
+
+# def detail_or_dev(request, product_slug, is_dev=False):
+    
