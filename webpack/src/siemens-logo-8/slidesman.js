3877b30ad214d24c0b8ead1330818a485fa322d3
Jaime Hernandez <jaimevazz@gmail.com>
2020-12-02 09:34:25 -0600

    SlidesManager: Get Camera positions from annotations and remove welcome slide

--- webpack/src/siemens-logo-8/slidesman.js
+++ webpack/src/siemens-logo-8/slidesman.js
@@ -4,19 +4,27 @@ import { slidesman  as params } from './params';
 export default class SlidesManager {
     constructor(camera) {
         this.camera = camera;
-        this.positions = params.positions;
+        this.firstTime = true;
+        this.offset = 0;
+        // this.positions = params.positions;
         $('#slidesCarousel').carousel({
             interval: false, //4500,
             keyboard: true,
             ride: true,
         });
         $('#slidesCarousel').on('slide.bs.carousel', (e) => {
-            // do something…
-            // console.log(e.to);
-            const p = this.positions[e.to];
-            const pv = new THREE.Vector3(p.x, p.y, p.z);
-            this.camera.position.copy(pv);
+            const to = e.to + this.offset;
+            // console.log(to);
+            const arr = annotations[to].camPosition.split(',');
+            this.camera.position.set(arr[0], arr[1], arr[2]);
             this.camera.lookAt(params.target.x, params.target.y, params.target.z);
+
+            // TODO: Maybe this breaks in somepoint
+            if (this.firstTime){
+                document.getElementById('welcome').remove();
+                this.firstTime = false;
+                this.offset = 1;
+            }           
         });
     }
 }
