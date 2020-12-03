3b01a68f8205618f65a7c0af2ee639ba0c868b44
Jaime Hernandez <jaimevazz@gmail.com>
2020-12-03 15:15:10 -0600

    Parametes from json data field (or file in dev)

--- webpack/src/siemens-logo-8/lightman.js
+++ webpack/src/siemens-logo-8/lightman.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import { lightman as params } from './params';
 
 export default class LightingManager {
     constructor(scene, renderer){
+        this.data = data.lightman;
         this.scene = scene;
         this.renderer = renderer;        
         this.addEnvironment();
@@ -15,31 +15,31 @@ export default class LightingManager {
         loader.load(assets.envmap, this.onTextureLoaded.bind(this));
     }
     addDirectional() {        
-        const dirLight = new THREE.DirectionalLight(params.dir.color, params.dir.intensity);
-        dirLight.position.set(params.dir.pos.x, params.dir.pos.y, params.dir.pos.z);
-        dirLight.castShadow = params.dir.shadow.enabled;
-        dirLight.shadow.mapSize.width = params.dir.shadow.mapSize;
-        dirLight.shadow.mapSize.height = params.dir.shadow.mapSize;
-        dirLight.shadow.camera.top = params.dir.shadow.top;
-        dirLight.shadow.camera.bottom = params.dir.shadow.bottom;
-        dirLight.shadow.camera.left = params.dir.shadow.left;
-        dirLight.shadow.camera.right = params.dir.shadow.right;
+        const dirLight = new THREE.DirectionalLight(this.data.dir.color, this.data.dir.intensity);
+        dirLight.position.set(this.data.dir.pos.x, this.data.dir.pos.y, this.data.dir.pos.z);
+        dirLight.castShadow = this.data.dir.shadow.enabled;
+        dirLight.shadow.mapSize.width = this.data.dir.shadow.mapSize;
+        dirLight.shadow.mapSize.height = this.data.dir.shadow.mapSize;
+        dirLight.shadow.camera.top = this.data.dir.shadow.top;
+        dirLight.shadow.camera.bottom = this.data.dir.shadow.bottom;
+        dirLight.shadow.camera.left = this.data.dir.shadow.left;
+        dirLight.shadow.camera.right = this.data.dir.shadow.right;
         this.scene.add(dirLight);
         // Shadow helper
-        if (params.dir.shadow.helper)
+        if (this.data.dir.shadow.helper)
             this.scene.add(new THREE.CameraHelper(dirLight.shadow.camera));           
     }
     addHemisphere() {
-        const hemiLight = new THREE.HemisphereLight(params.hemi.sky, params.hemi.ground, params.hemi.intensity);
-        hemiLight.position.set(params.hemi.pos.x, params.hemi.pos.y, params.hemi.pos.z);
+        const hemiLight = new THREE.HemisphereLight(this.data.hemi.sky, this.data.hemi.ground, this.data.hemi.intensity);
+        hemiLight.position.set(this.data.hemi.pos.x, this.data.hemi.pos.y, this.data.hemi.pos.z);
         this.scene.add(hemiLight);
     }
     onTextureLoaded(texture) {
         const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
         pmremGenerator.compileEquirectangularShader();        
         const envMap = pmremGenerator.fromEquirectangular(texture).texture;
-        this.scene.background = params.env.background ? envMap : null;
-        this.scene.environment = params.env.light ?  envMap : null;
+        this.scene.background = this.data.env.background ? envMap : null;
+        this.scene.environment = this.data.env.light ?  envMap : null;
         texture.dispose();
         pmremGenerator.dispose();
     }
